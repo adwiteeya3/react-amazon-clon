135edb43d484d37d7fb3cc24388152fe620004a7
Adwiteeya <adwiteeya3@gmail.com>
2020-10-31 15:10:30 +0530

    Home page update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="App">
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am the checkout page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-      {/* Header */}
-    </div>
   );
 }
 
