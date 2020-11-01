7cabbecee4d587f9d5e2f54dd9eddc4f9d2af9a9
Adwiteeya <adwiteeya3@gmail.com>
2020-11-02 00:10:07 +0530

    checkout

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+
+    const history = useHistory();
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='subtotal'>
             <CurrencyFormat
                 renderText={(value) => (
                     <>
                     <p>
-                        Subtotal ({basket.length} items):
-                        <strong>{` ${value}`}</strong>
+                    Subtotal ({basket.length} items): <strong>{value}</strong>
                     </p>
                     <small className='subtotal__gift'>
                         <input type='checkbox' /> This order contains a gift
@@ -24,9 +30,9 @@ function Subtotal() {
                 prefic={"₹"}
                 />
 
-                <button>Proceed to Checkout</button>
+                <button> Proceed to Checkout </button>
         </div>
     )
 }
 
-export default Subtotal
+export default Subtotal;
